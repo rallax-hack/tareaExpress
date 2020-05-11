f5f6a4d876fd250bde22eca28f0059d6336c41dd
rallax-hack <raul.lara.unam@gmail.com>
2020-05-11 17:55:00 -0500

    Ultima Actualizacion

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
-/*function strict(){
-  // Function-level strict mode syntax
-  'use strict';
-  function nested() { return "y yo también!"; }
-  return "Hola!  Soy una función de moda estricta!  " + nested();
-}
-function notStrict() { return "Yo no soy"; }
-*/
-// Whole-script strict mode syntax
+const express = require('express')
+const app = express()
 
-'use strict';
+app.get('/', function (req, res) {
+  res.send('SERVIDOR CORRIENDO CON EXPRESS')
+})
 
-module.exports = require('./lib/express');
+app.listen(3000)
 
-var app= require('express');
-
-
-var Http = require( 'http' );
-
-var server = Http.createServer(function(request,response){
-    //console.log('Alguien entró');
-    console.log(request.method);
-    console.log("URL:",request.url);
-    console.log("Headers:",request.headers);
-    response.writeHead(200, {'Content-Type': 'text/html'}); // cabecera http
-    response.write("<h1>Hola desde el node 3</h1>");// no se envía por que no se indica fin del documento
-    response.end(); // al fín se envía
-});
-
-server.listen( 3000, function( ) {
-console.log( 'Escuchando conexión en el puerto 3000' );
-});
+console.log("Mi primer server corriendo EXPRESS");
